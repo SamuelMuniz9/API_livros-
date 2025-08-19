a05bf89bdad72179c794489664d4a738639c5743
SamuelMuniz9 <msamuelmuniz@github.com>
2025-08-19 16:46:26 -0300

    commit_APi

--- app.py
+++ app.py
@@ -21,13 +21,26 @@ livros = [
 
 
 #metodo para pegar e exibir as informações de todos
-@app.route ('/livros')
+@app.route ('/livros', methods=['GET'])
 def get_livros():
     return jsonify(livros)
 
 #Consultar por id 
+@app.route('/livros/<int:id>', methods=['GET']) #tratando id na url
+def consultar_id(id):
+    for N in livros: 
+        if N.get('id') == id:  #buscando o id de cada livro e comparando com o id colocado na url
+            return jsonify(N) 
+         #AQUI ESTA SEM TRATAMENTO, ELE DA ERRO CASO COLOQUE UM ID QUE NAO EXISTE. 
+
+
+
 #Editar
+   #utilizar um request.get_json para editar e o method put que permite a ediçao e alem disso ele esta procurando o indice alem do id 
+   #update tambem é bom de utilizar 
+
 #Deletar
+ #aqui é delete mas acredito que seja a mesma estrutura que o put do editar 
 
 
 #URL DO SITE 
